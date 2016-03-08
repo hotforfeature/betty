557d69011e2dca96c59cbbcca3b11f16ca5f2560
Andrew Mitchell <abmitchell77@gmail.com>
2016-03-08 15:39:31 -0600

    Finished overview card (minus Stripe integration)

--- app/home/betty-overview.controller.js
+++ app/home/betty-overview.controller.js
@@ -2,7 +2,15 @@
   'use strict';
 
   function BettyOverviewController() {
+    // TODO: Add Stripe dialogs
 
+    this.showPayment = function showPayment() {
+
+    };
+
+    this.showFullPayment = function showFullPayment() {
+
+    };
   }
 
   angular.module('betty.home').controller('BettyOverviewController',
